cdb0366c68b93f92b148f0b4e68d83629c3fc047
Quentin Dubois <quentd96@gmail.com>
2024-09-02 10:33:08 +0200

    cleanup

--- build-api/api.py
+++ build-api/api.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 # Define a root `/` endpoint
 @app.get('/')
 def index():
     return 'Welcome to the Scorecast API!'
+
 
 @app.get('/predict')
 def predict():
