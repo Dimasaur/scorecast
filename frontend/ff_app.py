2297fc6a8d01fb9a8fb555573bbecb7888b3b421
Dima K <@users.noreply.github.com>
2024-09-03 14:41:19 +0200

    changed the path for cvs

--- frontend/ff_app.py
+++ frontend/ff_app.py
@@ -15,7 +15,7 @@ food_type = pd.read_csv('frontend/food_type.csv')
 #import state city dict
 state_city_path = '/Users/dima/code/Dimasaur/scorecast/frontend/state_city_dict.json'
 
-with open('state_city_dict.json') as json_file:
+with open('frontend/state_city_dict.json') as json_file:
     state_city_dict = json.load(json_file)
 
 st.markdown("""### First, select the cuisine type you'd like to have in your restaurants ###""")
